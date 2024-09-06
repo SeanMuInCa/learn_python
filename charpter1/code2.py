31f496c3c2f23817cf51c716592b7258767400f4
Sean <fengying1028@hotmail.com>
2024-09-05 19:19:25 -0600

    神奇的py

--- charpter1/code2.py
+++ charpter1/code2.py
@@ -1,5 +1,9 @@
 age = input("input your age: ")
 def check_age(age): return 2024 - int(age)
 
+year = check_age(age)
 print()
-print('you were born in ',check_age(age))
+print('you were born in ',year)
+if year > 2000: print('you are too young')
+else: print('you are old enough')
+
