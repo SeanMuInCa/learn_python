0fbab76c4be716346ca903114c4f65fc52fe1873
Sean <fengying1028@hotmail.com>
2024-09-06 14:38:14 -0600

    神奇的py

--- charpter1/code3.py
+++ charpter1/code3.py
@@ -1,3 +1,7 @@
 str = 'hello world'
 print(str[0])
-print(str[2:5])
+print(str[0:4])  # 从索引0开始，到索引4结束，但不包括索引4
+nums = '123456789'
+print(nums[0:9:2])  # 从索引0开始，到索引9结束，步长为2, 0和9可省略
+print(nums[::-1])
+print(str[::-1])  # 倒序 字符串反转
