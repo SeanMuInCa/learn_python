5db869fa1d35e6b8809e988407a2cebaaf2a610f
Sean <fengying1028@hotmail.com>
2024-09-06 15:08:54 -0600

    神奇的py

--- charpter1/code3.py
+++ charpter1/code3.py
@@ -5,3 +5,12 @@ nums = '123456789'
 print(nums[0:9:2])  # 从索引0开始，到索引9结束，步长为2, 0和9可省略
 print(nums[::-1])
 print(str[::-1])  # 倒序 字符串反转
+n1 = 1.12
+print(int(n1))
+
+if n1 > 2:
+    print('n1 > 2')
+elif n1 > 1:
+    print('n1 > 1')
+else:
+    print('n1 <= 1')
