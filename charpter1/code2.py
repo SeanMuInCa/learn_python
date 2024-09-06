52041301f6675787671ed5e5aa8a799d2edc1c63
Sean <fengying1028@hotmail.com>
2024-09-06 13:54:29 -0600

    神奇的py

--- charpter1/code2.py
+++ charpter1/code2.py
@@ -1,3 +1,6 @@
+import math
+from math import floor
+
 age = input("input your age: ")
 def check_age(age_input): return 2024 - int(age_input)
 
@@ -8,4 +11,10 @@ if year > 2000: print('you are too young')
 if year > 1990: print('you are old enough')
 else: print('you are too old')
 
+print(round(0.1 + 0.2, 2))
+print(math.floor(1.1 + 0.2))
+print(math.ceil(1.1 + 0.2))
+
+str = 'hello world'
+print(str.find('world'))
 
