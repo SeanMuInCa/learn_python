8ff2f0d46b5dae8fd8fcc2ef51e7fb7f5e10197e
Sean <fengying1028@hotmail.com>
2024-09-05 18:59:12 -0600

    神奇的py

--- charpter1/code1.py
+++ charpter1/code1.py
@@ -22,3 +22,12 @@ print('in another year' + str(year))  #这个有意思的拼接
 print('in another year', year)  #和上面的不一样,逗号自带空格效果
 
 
+print('today is %d, month %02d, %02d day %s' % (year, month, day, isinstance(year, str)))
+
+
+content = input("input something : ")
+print()
+print('the content is', content, end=' ')
+print("and it's instanceof str?", isinstance(content, str))
+print("and it's instanceof number?", isinstance(float(content), int), content)
+
