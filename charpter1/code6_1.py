e80fc76791250430f1c967eba989ad5e3da715f0
Sean <fengying1028@hotmail.com>
2024-09-16 14:53:03 -0600

    神奇的py

---
+++ charpter1/code6_1.py
@@ -0,0 +1,7 @@
+
+users = [
+    {'name': 'aaa', 'password': '123', 'status': True},
+    {'name': 'bbb', 'password': '123', 'status': True},
+    {'name': 'ccc', 'password': '123', 'status': False},
+]
+
