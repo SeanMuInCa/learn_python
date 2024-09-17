d053bf9bf3a4a9d6be063dcac3adc1001df6d01c
Sean <fengying1028@hotmail.com>
2024-09-16 18:16:20 -0600

    神奇的py

--- charpter1/code6_1.py
+++ charpter1/code6_1.py
@@ -8,16 +8,19 @@ RETRY = 3
 userName = input('请输入用户名：')
 password = input('请输入密码：')
 for i in users:
-    if userName == i['name'] and password == i['password'] and i['status']:
-            print('登录成功')
-    elif i['name'] == userName and i['password'] == password and not i['status']:
-            print('账号被冻结')
-    elif i['name'] == userName and i['password'] != password:
-        print('密码错误')
-        if i['status']:
-            RETRY -= 1
-            print('还有%d次机会' % RETRY)
+    if userName == i['name']:
+        if password == i['password']:
+            if i['status']:
+                print('登录成功')
+            else: print('账号被冻结')
         else:
-            print('账号被冻结')
-else:
-    print('用户名错误')
+            while RETRY > 1:
+                print('密码错误，还有%d次机会' % (RETRY - 1))
+                password = input('请输入密码：')
+                if password == i['password']:
+                    print('登录成功')
+                    break
+                else: RETRY -= 1
+            print('密码错误，账号冻结')
+        break
+else: print('用户名不存在')
