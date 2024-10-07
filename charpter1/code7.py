796da03eb503a529a22ff0dea1aabe8dc2e76f68
Sean <fengying1028@hotmail.com>
2024-10-07 10:50:31 -0600

    神奇的py

--- charpter1/code7.py
+++ charpter1/code7.py
@@ -4,3 +4,17 @@ def newf(*arg):
 newf(['a','b','c'])
 
 newf('aa','bb','cc')
+
+myfile = open('data.txt', 'r')
+
+mydata = myfile.read()
+mydict = {}
+
+for i in mydata.split():
+    if i in mydict:
+        mydict[i] += 1
+    else:
+        mydict[i] = 1
+
+
+print(mydict)
