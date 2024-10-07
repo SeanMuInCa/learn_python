376a7c5c5a8cfedd257d0eafbd8ed9035ad33e50
Sean <fengying1028@hotmail.com>
2024-10-07 11:09:38 -0600

    神奇的py

--- charpter1/code7.py
+++ charpter1/code7.py
@@ -1,3 +1,8 @@
+import random
+
+from jedi.inference.gradual.conversion import convert_names
+
+
 def newf(*arg):
     print(arg[0])
 
@@ -18,3 +23,12 @@ for i in mydata.split():
 
 
 print(mydict)
+
+func = lambda x: x+5
+print(list(map(func, [1,2,3,4,5,6])))
+
+names = ['Jared', 'Gavin', 'Walter', 'Mike', 'Brett', 'Hugh']
+
+convert_names = map(lambda x: random.choice(['Eagle', 'Hawk', 'Seagull', 'Heron', 'Sparrow', 'Raven']), names)
+
+print(list(convert_names))
