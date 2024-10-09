19395a41930c508365425f077fc56de7f449dbdc
Sean <fengying1028@hotmail.com>
2024-10-09 10:54:29 -0600

    神奇的py

--- charpter1/numpy.py
+++ charpter1/numpy.py
@@ -13,6 +13,14 @@ print(a)
 print(a.shape)
 print(a.ndim)
 print(a.dtype.name)
+# size in bytes 64bites = 8 bytes
 print(a.itemsize)
 print(a.size)
 print(type(a))
+print(a[1])
+print(a[1,])
+# row 1 and all of things in row 1
+print(a[1, :])
+# all the rows but the col is 2
+print(a[:, 2])
+print(a[0, 2])
