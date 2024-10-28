f2edbb7b3c6823f9c9cd05eba0a5afca1287d0cd
Sean <fengying1028@hotmail.com>
2024-10-28 09:34:59 -0600

    神奇的py

--- Class/pandas2.py
+++ Class/pandas2.py
@@ -1,19 +1,22 @@
 import pandas as pd
-
-my_series = pd.Series([
-4.6, 2.1, -4.0, 3.0])
+from pandas import Series, DataFrame
+my_series = Series([
+4.6, 2.1, -4.0, 3.0],['a','b','c','d'])
 
 print(my_series)
-df1 = pd.DataFrame()
+print('-'*100)
+df1 = DataFrame()
 print(df1)
-df1 = pd.DataFrame(columns=('Col 1', 'Col 2', 'Col3'))
+print('-'*100)
+df1 = DataFrame(columns=('Col 1', 'Col 2', 'Col3'))
 print(df1)
-df1 = pd.DataFrame([[1,2,3],[3,4,5]],columns=list(('Col 1', 'Col 2', 'Col3')))
+print('-'*100)
+df1 = DataFrame([[1,2,3],[3,4,5]],columns=list(('Col 1', 'Col 2', 'Col3')))
 print(df1.shape)
 
-a = pd.DataFrame([[1,2,3],[3,4,5]], columns=list('ABC'))
-b = pd.DataFrame([[5,2,3],[7,4,5]], columns=list('BDE'))
-c = pd.DataFrame([[11,12,13],[17,14,15]], columns=list('XYZ'))
+a = DataFrame([[1,2,3],[3,4,5]], columns=list('ABC'))
+b = DataFrame([[5,2,3],[7,4,5]], columns=list('BDE'))
+c = DataFrame([[11,12,13],[17,14,15]], columns=list('XYZ'))
 
 print(a)
 print(b)
