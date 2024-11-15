1b4564e3544a9ea824bc62974d12ecc44ba111bd
Sean <fengying1028@hotmail.com>
2024-11-15 17:21:59 -0600

    神奇的py

--- assignment/getDataFromYoutube.py
+++ assignment/getDataFromYoutube.py
@@ -1,4 +1,6 @@
 import os
+from distutils.log import fatal
+from operator import index
 from re import search
 
 import pandas as pd
@@ -10,4 +12,19 @@ youtube = build('youtube', 'v3', developerKey=API_KEY)
 
 search_keyword = 'china'
 search_response = youtube.search().list(q=search_keyword, part='id,snippet', maxResults=10).execute()
-search_result = search_response['items']
+search_results = search_response['items']
+
+videos = []
+for search_result in search_results:
+    if search_result['id']['kind'] == 'youtube#video':
+        video = {
+            'id': search_result['id']['videoId'],
+            'title': search_result['snippet']['title'],
+            'description': search_result['snippet']['description'],
+            'publishedAt': search_result['snippet']['publishedAt'],
+            'channelId': search_result['snippet']['channelId'],
+            'channelTitle': search_result['snippet']['channelTitle'],
+        }
+        videos.append(video)
+df = pd.DataFrame(videos)
+df.to_csv('dataFromYoutube.csv', index=False)
