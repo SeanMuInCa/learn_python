3b4c3398471888b180a7d7d33b389807e6e7a274
Sean <fengying1028@hotmail.com>
2024-11-14 21:14:04 -0600

    神奇的py

--- assignment/gameSales.py
+++ assignment/gameSales.py
@@ -19,6 +19,17 @@ gameSalesCleaned = gameSales.dropna().copy()
 
 # Add a new column '21 Central' based on a condition
 gameSalesCleaned['21 Central'] = np.where(gameSalesCleaned['Year'] > 2000, '21st', '20th')
-
-# Display the first few rows of the cleaned DataFrame
-print(gameSalesCleaned.head())
+print('-' * 100)
+# Step 3: Perform exploratory analysis & visualization
+# calculate mean values
+GlobalSalesMeanValues = gameSalesCleaned['Global_Sales'].mean()
+print(f'The mean value of Global Sales is: {GlobalSalesMeanValues} Millions')
+print('-' * 100)
+# calculate range values
+NorthAmericaSalesRange = gameSalesCleaned['NA_Sales'].max() - gameSalesCleaned['NA_Sales'].min()
+print(f'The range of North America Sales is: {NorthAmericaSalesRange} Millions')
+print('-' * 100)
+# calculate sum values of Japan sales
+SumJapanSales = gameSalesCleaned['JP_Sales'].sum()
+print(f'The sum of Japan Sales is: {SumJapanSales} Millions')
+print('-' * 100)
