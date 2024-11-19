90af8977c5d0fe030f63a34a930db2a17c7ed625
Sean <fengying1028@hotmail.com>
2024-11-18 23:33:23 -0600

    神奇的py

--- Class/practice_1118.py
+++ Class/practice_1118.py
@@ -4,9 +4,16 @@ import numpy as np
 arr = np.array([1,2,3,4])
 print(arr, type(arr),arr.ndim)
 print(arr.shape)
-arr.shape = (2,2)
-print(arr)
-print(arr, type(arr),arr.ndim)
+
 
 arr1 = np.array([[1,2,3],[4,5,6]])
 print(arr1.ndim)
+
+arr2 = arr * 3
+print(arr)
+print(arr2)
+print(arr + arr2)
+print(arr.ndim)
+arr3 = np.array([1,'233',{1}])
+print(arr3)
+print(arr3.dtype)
